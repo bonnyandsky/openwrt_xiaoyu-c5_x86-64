b82a4f4c3fccbfd155c0a143dd59ff76533311e8
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-20 14:16:19 +0800

    Update x86_64-mysetting_ipv6_2in1-part2.sh

--- x86_64-mysetting_ipv6_2in1-part2.sh
+++ x86_64-mysetting_ipv6_2in1-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+cp -r package/luci-app-netdata package/lean/
