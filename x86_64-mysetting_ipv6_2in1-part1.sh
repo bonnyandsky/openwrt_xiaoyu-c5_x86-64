8c61ba3232e515445f245a4ee6253439927a5aa9
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-20 12:53:57 +0800

    Update x86_64-mysetting_ipv6_2in1-part1.sh

--- x86_64-mysetting_ipv6_2in1-part1.sh
+++ x86_64-mysetting_ipv6_2in1-part1.sh
@@ -16,9 +16,6 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
 # echo 'src-git opentopd https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
