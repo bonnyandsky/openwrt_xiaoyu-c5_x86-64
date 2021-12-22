1e1977adf399f2ec119d1af33638f234608e2dd4
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-22 08:30:08 +0800

    Update xiaoyu_c5-mysetting_ipv6_2in1-part2.sh

--- xiaoyu_c5-mysetting_ipv6_2in1-part2.sh
+++ xiaoyu_c5-mysetting_ipv6_2in1-part2.sh
@@ -16,6 +16,7 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 # 移除重复软件包
 rm -rf package/lean/luci-app-netdata
 rm -rf package/lean/luci-app-wrtbwmon
+rm -rf package/lean/luci-theme-argon
 
 # 添加额外软件包，不在根目录要用svn co，然后tree/main替换成trunk
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
@@ -26,6 +27,10 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest 
 # 流量监控
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon package/wrtbwmon
+
+# Themes
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
