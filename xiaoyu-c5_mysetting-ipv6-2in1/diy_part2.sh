0fce682770bf1d41452e67df9a0153eb261cfb49
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-28 17:37:12 +0800

    Update diy_part2.sh

--- xiaoyu-c5_mysetting-ipv6-2in1/diy_part2.sh
+++ xiaoyu-c5_mysetting-ipv6-2in1/diy_part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 移除重复软件包
 rm -rf package/lean/luci-app-netdata
