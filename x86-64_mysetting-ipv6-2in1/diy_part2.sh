12e5d4445e5d517c2f1051829db2de341719e498
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2022-01-04 11:05:42 +0800

    Update diy_part2.sh

--- x86-64_mysetting-ipv6-2in1/diy_part2.sh
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# 删除默认密码
+# 删除默认密码 第二条可以起作用，但是在immortalwrt不起作用。第一条可以用于immortalwrt
 #sed -i '34s/sed/# sed/g' package/lean/default-settings/files/zzz-default-settings
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
